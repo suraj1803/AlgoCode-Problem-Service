b6bf49e875f18b342ce771811b057f06b0c83088
Suraj Biswas <surajbiswas182003@gmail.com>
2025-11-02 22:04:05 +0530

    Fix: migrated from numeric status codes to http-status-codes library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,17 +1,46 @@
+import { StatusCodes } from "http-status-codes";
 export function pingProblemController(req, res) {
   res.json({ message: "Problem controller is up" });
 }
 
-export function addProblem(req, res) { }
+export function addProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
 
-export function getProblem(req, res) { }
+export function getProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
 
-export function getProblems(req, res) { }
+export function getProblems(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
 
-export function deleteProblem(req, res) { }
+export function deleteProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
 
-export function deleteProblems(req, res) { }
+export function deleteProblems(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
 
-export function updateProblem(req, res) { }
+export function updateProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
 
-export function updateProblems(req, res) { }
+export function updateProblems(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
